f62d493cf0533f92a912c9faf08aad846a66a464
Daniel Gruici <dgruici@gmail.com>
2025-05-31 11:42:42 -0700

    JENI-2: added instrumentation call for context so we could verify the game state with the actual string. then if someone changes the string in the future, it'll fail and we can update the test.

--- app/src/androidTest/java/com/example/myapplication/GameScreenTest.kt
+++ app/src/androidTest/java/com/example/myapplication/GameScreenTest.kt
@@ -1,12 +1,15 @@
 package com.example.myapplication
 
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -32,23 +35,27 @@ class GameScreenTest {
 
     @Test
     fun testGameOver() {
-        // TODO - initialize the screen with the final view state
-        // verify that game_end_message displayed
+        val testGameState = GameUiState(
+            gameEnded = true,
+            winner = "White"
+        )
+
         composeTestRule.setContent {
             MyApplicationTheme {
                 GameScreen(
                     WindowWidthSizeClass.Medium,
                     GameViewModel(
-                        GameUiState(
-                            gameEnded = true,
-                            winner = "White"
-                        )
+                        testGameState
                     )
                 )
             }
         }
 
-        composeTestRule.onNodeWithText("Game ended", substring = true).assertIsDisplayed()
+        val winnerText = getInstrumentation().targetContext.getString(
+            R.string.game_end_message,
+            testGameState.winner
+        )
+        composeTestRule.onNodeWithText(winnerText).assertIsDisplayed()
     }
 
 }
